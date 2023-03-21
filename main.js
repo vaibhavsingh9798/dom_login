19ccbbc3c5d86ca40128bea237bb02b15e8f7541
Vaibhav <vaibhavsinghvrajpot@gmail.com>
2023-03-21 15:49:07 +0530

    read and display user from localstorage

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ let form = document.getElementById('form')
 
 form.addEventListener('submit',onSubmit)
 
+let count = 0;
 function onSubmit(e){
     e.preventDefault()
     let name =  document.getElementById('name').value
     let email = document.getElementById('email').value
+    let phone = document.getElementById('phone').value
     // create error msg 
     let p = document.createElement('p')
     p.appendChild(document.createTextNode('please enter value'))
@@ -25,21 +27,36 @@ function onSubmit(e){
    {
     // add current user in page
     let li = document.createElement('li')
-    li.appendChild(document.createTextNode(`${name} : ${email}`))
+    li.appendChild(document.createTextNode(`${name} : ${email} - ${phone}`))
     ul.appendChild(li)
 
     // add curent user as object in local storege
    
-     let objName = {name,email}
+     let objName = {name,email,phone}
     // convert object to string then store
-     localStorage.setItem(`${name}`,JSON.stringify(objName))
+     localStorage.setItem(`${email}`,JSON.stringify(objName))
      // convert string to object then print
-     console.log( JSON.parse(localStorage.getItem(name)))
-
+     console.log( JSON.parse(localStorage.getItem(email)))
+     console.log(localStorage.key(3))
+     count++
    }
-   
-
-   document.getElementById('name').value=''
+     document.getElementById('name').value=''
      document.getElementById('email').value=''
-   
+     document.getElementById('phone').value=''
+ 
+}
+if(count == 0){
+  for(let i=1;i<10;i++){
+    let key = localStorage.key(i)
+    if(!key)
+    break;
+    let data = JSON.parse(localStorage.getItem(key))
+    let name = data.name;
+    let email = data.email;
+    let phone = data.phone;
+    let ul = document.getElementById('items')
+    let li = document.createElement('li')
+    li.appendChild(document.createTextNode(`${name} : ${email} - ${phone}`))
+    ul.appendChild(li) 
+  }
 }
